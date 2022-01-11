45fab5c5b775c1c3c2dd0e9440b3600db359fd5d
Erloys <jeankevin221@gmail.com>
2022-01-11 10:42:31 +0400

    minimal bot

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+
+import discord
+from dotenv import load_dotenv
 
 DIR_PATH = "/data/projets/genshinImage/bot/"
 
-load_dotenv(find_dotenv(DIR_PATH + ".env") )
+load_dotenv()
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print(f'currently logged as {client.user}')
+
+
+
+@client.event
+async def on_message(m):
+	if m.author == client.user:
+		return
+	
+	if m.content.startswith('$hello'):
+		await m.channel.send('Hi !')
+
+
+client.run(os.environ['TOKEN'])
+
+
